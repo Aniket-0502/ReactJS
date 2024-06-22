99c31e10f9678169eecee14de109089b33c10254
Aniket-0502 <134960557+Aniket-0502@users.noreply.github.com>
2024-06-22 16:16:10 +0530

    store configured

--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,3 +1,5 @@
+// console.log(process.env.REACT_APP_APPWRITE_URL) -> used when we build react using create react-app, but in this case we are using vite, thus the process to get environment varibles is different
+
 const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
